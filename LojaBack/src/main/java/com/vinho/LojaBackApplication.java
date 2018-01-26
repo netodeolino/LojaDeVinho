3c6d8b9542240a8167914e4bfa3501edc4db358e
Neto Deolino <netodeolino@gmail.com>
2018-01-26 15:51:13 -0300

    criando projetos

---
+++ LojaBack/src/main/java/com/vinho/LojaBackApplication.java
@@ -0,0 +1,12 @@
+package com.vinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaBackApplication.class, args);
+	}
+}
